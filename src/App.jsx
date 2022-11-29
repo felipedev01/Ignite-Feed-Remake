331634c6fe17575336ae903a398b20bcfdb22907
Felipe <felipecss88@gmail.com>
2022-11-29 09:14:54 -0300

    Component post criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import { Header } from './Components/Header'
+import { Post } from './Components/Post'
 import './global.css'
 
 export function App() {
   
 
   return (
-    <Header/>
+    <div>
+      <Header/>
+       <Post author="Felipe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+"/>
+<Post author="Felipe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+"/>
+    </div>
+    
+
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,16 @@
 import styles from './Header.module.css'
 import igniteLogo from '../assets/Ignite simbol.svg'
 
-console.log(igniteLogo)
+
 
 export function Header(){
 
     return(
 
         <header className={styles.header}>
-              <strong >Ignite Feed</strong>
+
+            <img src="/src/assets/Ignite simbol.svg" alt="Logo ignite" />
+              
 
         </header>
         
