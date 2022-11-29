e2bb8e76100df4e10815c7cbfd38e72d6552a89f
Felipe <felipecss88@gmail.com>
2022-11-29 10:08:04 -0300

    Wrapper criado

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,40 @@ import { useState } from 'react'
 import { Header } from './Components/Header'
 import { Post } from './Components/Post'
 import './global.css'
+import styles from '../src/App.module.css'
 
 export function App() {
   
 
   return (
     <div>
-      <Header/>
-       <Post author="Felipe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
-"/>
-<Post author="Felipe" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
-"/>
+    <Header/>
+
+    <div className={styles.Wrapper}>
+      
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+        <Post author="Felipe" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+        "/>
+        <Post author="Felipe" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+      "/>
+      <Post author="Felipe" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+      "/>
+
+         <Post author="Felipe" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis odit placeat sequi alias quia consequatur natus deserunt. Accusamus et magnam illo recusandae reprehenderit illum suscipit ut libero ea fugiat.
+        "/>
+
+        </main>
+       
     </div>
+    </div>
+
     
 
   )
