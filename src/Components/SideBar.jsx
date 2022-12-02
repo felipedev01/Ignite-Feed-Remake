a75f22175521c3fd79dc54b78d50aa33c4cfcd3f
Felipe <felipecss88@gmail.com>
2022-12-02 07:07:04 -0300

    SideBar footer estilizado

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,7 +4,17 @@ export function SideBar(){
 
     return(
         <aside className={styles.SideBar}>
-           side bar
+         <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1669087866031-d63345007809?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+         <div className={styles.profile}>
+            <strong>Felipe</strong>
+            <span>Web Developer</span>
+
+         </div>
+         <footer>
+            <a href="#">
+                Edtar meu perfil
+            </a>
+         </footer>
         </aside>
     )
 }
