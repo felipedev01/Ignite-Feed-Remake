a68bdc55af0a18a2da7d526377243af3070ad89f
Felipe <felipecss88@gmail.com>
2022-12-02 07:33:44 -0300

    imagem perfil adicionada

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -6,6 +6,7 @@ export function SideBar(){
         <aside className={styles.SideBar}>
          <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1669087866031-d63345007809?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
          <div className={styles.profile}>
+            <img className={styles.Avatar} src="https://avatars.githubusercontent.com/u/81122606?v=4" />
             <strong>Felipe</strong>
             <span>Web Developer</span>
 
